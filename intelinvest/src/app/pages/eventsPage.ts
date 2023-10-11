5686ba43ca2a0b3f483c9c7a5ff098ba6c60a214
Anton Svetz <guluykanton@gmail.com>
2023-10-11 16:25:01 +0400

    Добавление отображания данных в таблице, исправление общей конфигурации

--- intelinvest/src/app/pages/eventsPage.ts
+++ intelinvest/src/app/pages/eventsPage.ts
@@ -1,32 +1,46 @@
-import {Component, UI} from "@intelinvest/platform/src/app/ui";
+import { Component, UI } from "@intelinvest/platform/src/app/ui";
 
 @Component({
-    // language=Vue
-    template: `
-          <v-container fluid class="selectable">
-          <template label="Delete me in realization">
-                Events page
-                events size: {{ events.length }}<br/>
-                <pre>
-                    todo вывести таблицу с событиями, колонки: Дата (date), Сумма (totalAmount), Количество (quantity), Название (label), Комментарий (comment), Период (period)
-                </pre>
-          </template>
-          <!-- todo вывести таблицу с событиями, колонки: Дата (date), Сумма (totalAmount), Количество (quantity), Название (label), Комментарий (comment), Период (period) -->
-          </v-container>
-    `
+  // language=Vue
+  template: `
+    <v-container fluid class="selectable">
+      <v-data-table
+        :headers="headers"
+        :items="events"
+        class="elevation-1"
+        :items-per-page="10"
+      >
+        <template v-slot:item.date="{ item }">{{ item.date }}</template>
+        <template v-slot:item.totalAmount="{ item }">{{
+          item.totalAmount
+        }}</template>
+        <template v-slot:item.quantity="{ item }">{{ item.quantity }}</template>
+        <template v-slot:item.label="{ item }">{{ item.label }}</template>
+        <template v-slot:item.comment="{ item }">{{ item.comment }}</template>
+        <template v-slot:item.period="{ item }">{{ item.period }}</template>
+      </v-data-table>
+    </v-container>
+  `,
 })
 export class EventsPage extends UI {
+  private events: any = [];
+  private headers = [
+    { text: "Дата", value: "date" },
+    { text: "Сумма", value: "totalAmount" },
+    { text: "Количество", value: "quantity" },
+    { text: "Название", value: "label" },
+    { text: "Комментарий", value: "comment" },
+    { text: "Период", value: "period" },
+  ];
 
-    private events: any = [];
-
-    async created(): Promise<void> {
-        const params = {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json;charset=UTF-8"
-            }
-        };
-        const response = await fetch("http://localhost:3004/events", params);
-        this.events = await response.json();
-    }
+  async created(): Promise<void> {
+    const params = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+    };
+    const response = await fetch("http://localhost:3004/events", params);
+    this.events = await response.json();
+  }
 }
--- intelinvest/webpack.config.js
+++ intelinvest/webpack.config.js
@@ -2,37 +2,40 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-    devServer: {
-        historyApiFallback: true,
-        noInfo: true,
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8080,
+    open: true,
+    historyApiFallback: true,
+    noInfo: false,
+  },
+  devtool: "eval-source-map",
+  entry: "./src/index.ts",
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        loader: "ts-loader",
+        test: /\.ts$/,
+        options: {},
+      },
+    ],
+  },
+  output: {
+    filename: "build.js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/dist/",
+  },
+  performance: {
+    hints: false,
+  },
+  plugins: [],
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.esm.js",
     },
-    devtool: "eval-source-map",
-    entry: "./src/index.ts",
-    mode: process.env.NODE_ENV,
-    module: {
-        rules: [
-            {
-                exclude: /node_modules/,
-                loader: "ts-loader",
-                test: /\.ts$/,
-                options: {
-                }
-            }
-        ]
-    },
-    output: {
-        filename: "build.js",
-        path: path.resolve(__dirname, "./dist"),
-        publicPath: "/dist/",
-    },
-    performance: {
-        hints: false,
-    },
-    plugins: [],
-    resolve: {
-        alias: {
-            "vue$": "vue/dist/vue.esm.js",
-        },
-        extensions: [".ts", ".js", ".json"],
-    },
+    extensions: [".ts", ".js", ".json"],
+  },
 };
